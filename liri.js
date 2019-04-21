72f8e4e9534acb36c003b46c35b82af0abba44ea
ppatel662 <phpatel662@gmail.com>
2019-04-21 10:31:14 -0400

    starting

--- liri.js
+++ liri.js
@@ -1,13 +1,68 @@
-var axios = require("axios");
-var movieInfo = process.argv[2];
-var movie = process.argv[3];
+//Read and set any environment variables with the .env package.
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-console.log(queryUrl);
+//Require data from File System npm package
+var fs = require("fs");
 
-axios.get(queryUrl).then(
-    fuction(response) {
-        console.log(response)
-    }
-);
+
+// Requiring our Spotify function exported from spotify.js
+
+// Requiring our Movies function exported from omdb.js
+var myMovies = require("./movies.js");
+// Requiring our Movies function exported from omdb.js
+var myConcert = require("./concerts.js");
+
+//Creates initial user command.
+var userCommand=process.argv[2];
+//Creates user input.
+var userInput=process.argv.splice(3,process.argv.length).join(' ');
+
+
+//Program conditions 
+switch (userCommand) {
+    // help function to clarify commands used
+    case "help":
+        console.log("Please type one of these commands\n"+
+                    "'concert-this': to search your favorite artist concerts\n"+
+                    
+                    "'movie-this': to search your favorite movie \n"+
+                    "'do-what-it-says': using command from random.txt \n"
+                    );
+        break;
+    case "concert-this":
+        myConcert(userInput);
+        break;
+    case "movie-this":
+        myMovies(userInput);
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+    //if anything else written
+    default:
+        console.log("LIRI doesn't understand that - Please type 'node liri.js help' for more information");
+};
+
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        //Return error if error occurs.
+        if (error) {
+            return console.log(error);
+        }
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+        
+        // Each command is represented. Because of the format in the txt file, remove the quotes to run these commands. 
+
+        if (dataArr[0] === "concert-this") {
+            var venueName = dataArr[1].slice(1, -1);
+            console.log("Venue Name: "+venueName)
+            myConcert(venueName);
+        } else if(dataArr[0] === "movie-this") {
+            var movieName = dataArr[1].slice(1, -1);
+            console.log("Movie Name: "+movieName)
+            myMovies(movieName);
+        }
+    });
+};
